27a7bedef8e52fe888459ebacd88a30250869eb4
Ashutosh <ashutoshkumar966756@gmail.com>
2025-08-11 14:19:36 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+const scrollReveloption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".container .letter-s", {
+  duration: 1000,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".container img", {
+  duration: 1000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".container .text_left",{
+    ...scrollReveloption,
+    origin: "right",
+    delay:2000,
+});
+
+ScrollReveal().reveal(".container .text_right",{
+    ...scrollReveloption,
+    origin: "left",
+    delay:2000,
+});
+
+ScrollReveal().reveal(".container .explore", {
+  duration: 1000,
+  delay: 2500,
+});
+
+ScrollReveal().reveal(".container .catalog", {
+  duration: 1000,
+  delay: 3500,
+});
+
+ScrollReveal().reveal(".container .print", {
+  duration: 1000,
+  delay: 4000,
+});
+
+ScrollReveal().reveal(".container h5", {
+  duration: 1000,
+  interval:500,
+  delay: 3000,
+});
+
+
